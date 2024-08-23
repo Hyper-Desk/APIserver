fd0866006c8b93f1ad3f7a42730eefd088ab8abb
choish19 <100904838+choish19@users.noreply.github.com>
2024-08-24 00:58:58 +0900

    [Fix] cors에러 nginx에서 처리로 변경 (#19)

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -12,19 +12,6 @@ import (
 func Run(address string) (chan error, error) {
 	router := gin.Default()
 
-	router.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://hyperdesk.minboy.duckdns.org")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-
-		c.Next()
-	})
-
-	router.OPTIONS("/*wildcard", func(c *gin.Context) {
-		c.Status(200)
-	})
-
 	v1 := router.Group("/api/user")
 	user_rest.UserRoutes(v1)
 

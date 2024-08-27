10a65922075a6431b60e3b4d64f1e56779d91c5c
choish19 <100904838+choish19@users.noreply.github.com>
2024-08-28 03:58:13 +0900

    [Fix] storage 조회 API iso 정보 삭제, iso 조회 API 수정 (#22)

--- src/proxmox/rest/storage_service.go
+++ src/proxmox/rest/storage_service.go
@@ -15,46 +15,27 @@ func fetchStorage(node string, token models.ProxmoxToken, proxy models.Proxy) (*
 		return nil, err
 	}
 
-	var diskStorage []interface{}
-	var isoStorage []interface{}
-
-	for _, storage := range storageData {
-		storageMap, ok := storage.(map[string]interface{})
-		if !ok {
-			continue
-		}
-
-		if avail, exists := storageMap["avail"].(float64); exists {
-			storageMap["avail"] = fmt.Sprintf("%.2f", avail/(1024*1024*1024))
-		}
-		if total, exists := storageMap["total"].(float64); exists {
-			storageMap["total"] = fmt.Sprintf("%.2f", total/(1024*1024*1024))
-		}
-		if used, exists := storageMap["used"].(float64); exists {
-			storageMap["used"] = fmt.Sprintf("%.2f", used/(1024*1024*1024))
-		}
-
-		if content, exists := storageMap["content"].(string); exists {
-			if strings.Contains(content, "images") {
-				diskStorage = append(diskStorage, storageMap)
-			}
-			if strings.Contains(content, "iso") {
-				isoStorage = append(isoStorage, storageMap)
-			}
-		}
-	}
+	contentType := "images"
+	storage := fetchStorageByContent(storageData, contentType)
 
 	return &models.StorageList{
-		DiskStorage: diskStorage,
-		IsoStorage:  isoStorage,
+		DiskStorage: storage,
 	}, nil
 }
 
 func fetchIsos(node string, token models.ProxmoxToken, proxy models.Proxy) ([]map[string]interface{}, error) {
-	storageData, _ := fetchStorage(node, token, proxy)
+	url := fmt.Sprintf("https://%s:%s/api2/json/nodes/%s/storage", proxy.Address, proxy.Port, node)
+
+	storageData, err := fetchProxmoxDataForURL(token, url, "GET")
+	if err != nil {
+		return nil, err
+	}
+
+	contentType := "iso"
+	isoStorage := fetchStorageByContent(storageData, contentType)
 
 	var isoData []map[string]interface{}
-	for _, storage := range storageData.IsoStorage {
+	for _, storage := range isoStorage {
 		storageMap, ok := storage.(map[string]interface{})
 		if !ok {
 			continue
@@ -74,13 +55,53 @@ func fetchIsos(node string, token models.ProxmoxToken, proxy models.Proxy) ([]ma
 }
 
 func fetchIso(node string, storage string, token models.ProxmoxToken, proxy models.Proxy) ([]interface{}, error) {
-
 	url := fmt.Sprintf("https://%s:%s/api2/json/nodes/%s/storage/%s/content", proxy.Address, proxy.Port, node, storage)
 
-	isoData, err := fetchProxmoxDataForURL(token, url, "GET")
+	isoStorageData, err := fetchProxmoxDataForURL(token, url, "GET")
 	if err != nil {
 		return nil, err
 	}
 
-	return isoData, nil
+	var filteredIsoData []interface{}
+	for _, iso := range isoStorageData {
+		isoMap, ok := iso.(map[string]interface{})
+		if !ok {
+			continue
+		}
+		if content, exists := isoMap["content"].(string); exists {
+			if strings.Contains(content, "iso") {
+				filteredIsoData = append(filteredIsoData, iso)
+			}
+		}
+	}
+
+	return filteredIsoData, nil
+}
+
+func fetchStorageByContent(storageData []interface{}, contentType string) []interface{} {
+	var retStorage []interface{}
+
+	for _, storage := range storageData {
+		storageMap, ok := storage.(map[string]interface{})
+		if !ok {
+			continue
+		}
+
+		if avail, exists := storageMap["avail"].(float64); exists {
+			storageMap["avail"] = fmt.Sprintf("%.2f", avail/(1024*1024*1024))
+		}
+		if total, exists := storageMap["total"].(float64); exists {
+			storageMap["total"] = fmt.Sprintf("%.2f", total/(1024*1024*1024))
+		}
+		if used, exists := storageMap["used"].(float64); exists {
+			storageMap["used"] = fmt.Sprintf("%.2f", used/(1024*1024*1024))
+		}
+
+		if content, exists := storageMap["content"].(string); exists {
+			if strings.Contains(content, contentType) {
+				retStorage = append(retStorage, storageMap)
+			}
+		}
+	}
+	return retStorage
 }
